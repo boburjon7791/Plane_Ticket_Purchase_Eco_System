054a2f3b43326dfd6d5efb3b7fc3978394f2c1f8
Boburjon <soliyevboburjon95@gmail.com>
2023-09-02 07:00:49 +0500

    This is my first big project that made with Spring Boot

--- src/main/java/com/example/demo/util/BaseUtil.java
+++ src/main/java/com/example/demo/util/BaseUtil.java
@@ -1,7 +1,7 @@
 package com.example.demo.util;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.*;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,10 +9,13 @@ import java.util.Set;
 public class BaseUtil {
     public static Collection<String> cities = new HashSet<>();
     static {
-        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/cities.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                cities.add(line);
+        try (InputStream is = BaseUtil.class.getClassLoader().getResourceAsStream("application-cities.properties")) {
+            assert is != null;
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    cities.add(line);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
