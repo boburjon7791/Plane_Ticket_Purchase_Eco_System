eddb5ad7ef7a2cd68d527d0af9ee8349c71a2769
Boburjon <soliyevboburjon95@gmail.com>
2023-09-28 18:58:57 +0500

    This is my first big project that made with Spring Boot

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -51,7 +51,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-   @Bean
+//   @Bean
    public CommandLineRunner runner(){
 	   return args -> {
 		   try {
