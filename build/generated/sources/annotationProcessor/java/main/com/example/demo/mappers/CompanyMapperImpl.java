2ccf01684fefe722e56dd11346b23e935898cc49
Boburjon <soliyevboburjon95@gmail.com>
2023-09-28 16:43:59 +0500

    This is my first big project that made with Spring Boot

---
+++ build/generated/sources/annotationProcessor/java/main/com/example/demo/mappers/CompanyMapperImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-09-26T18:01:52+0500",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.2.1.jar, environment: Java 17.0.2 (Oracle Corporation)"
+)
+@Component
+public class CompanyMapperImpl implements CompanyMapper {
+}
